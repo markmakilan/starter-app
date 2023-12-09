0cd568eeab079b0fcb7153076c743ede029bb1db
Mark Makilan <mark.makilan@gets.ph>
2023-12-09 13:11:34 +0800

    change primary color

--- resources/views/livewire/pages/auth/verify-email.blade.php
+++ resources/views/livewire/pages/auth/verify-email.blade.php
@@ -48,7 +48,7 @@ $logout = function (Logout $logout) {
             {{ __('Resend Verification Email') }}
         </x-primary-button>
 
-        <button wire:click="logout" type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button wire:click="logout" type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
             {{ __('Log Out') }}
         </button>
     </div>
