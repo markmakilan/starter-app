901d4b700fe1f74ac4a089a8a729e4916c9efae3
Mark Makilan <mark.makilan@gets.ph>
2023-12-09 13:01:12 +0800

    Initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate([
+            'family_name' => 'Dela Cruz',
+            'given_name' => 'Juan',
+            'email' => 'sysadmin@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now()
+        ]);
+    }
+}
