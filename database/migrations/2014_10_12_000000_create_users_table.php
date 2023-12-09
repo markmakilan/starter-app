fedd0384c422fb0d646d36793d4b5a5a84597358
Mark Makilan <mark.makilan@gets.ph>
2023-12-09 11:56:47 +0800

    Initial commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('family_name');
+            $table->string('given_name');
+            $table->string('middle_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
