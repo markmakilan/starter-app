84c16b5bde4ab2743bdeeee9f58f130a8ee2fdb6
Mark Makilan <mark.makilan@gets.ph>
2023-12-14 14:39:05 +0800

    updated scafolding

--- resources/views/livewire/pages/admin/user/index.blade.php
+++ resources/views/livewire/pages/admin/user/index.blade.php
@@ -1,10 +1,23 @@
 <div>
-    <div class="flex items-center justify-between py-1.5">
-        <h1 class="text-xl text-gray-900 font-semibold">Users</h1>
+    <div class="flex items-center justify-between py-1.5 mb-1">
+        <x-input-label class="text-xl font-semibold">Users</x-input-label>
         <div class="flex items-center gap-2">
             <a wire:navigate href="/admin/dashboard" class="text-sm text-gray-400">Dashboard</a>
             <span class="text-sm text-gray-400">/</span>
             <span class="text-sm text-blue-400">Users</span>
         </div>
     </div>
+
+    <x-ui.card>
+        <x-division.flexbox>
+            <x-division.flexbox>
+                <x-icon.search class="w-4 h-4" />
+                <x-text-input />
+            </x-division.flexbox>
+            
+            <x-primary-button>
+                <span>Search</span>
+            </x-primary-button>
+        </x-division.flexbox>
+    </x-ui.card>
 </div>
