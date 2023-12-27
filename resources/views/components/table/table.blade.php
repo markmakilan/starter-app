3ae5abef816df299d1e410dff7bd3711d1770c98
Mark Makilan <mark.makilan@gets.ph>
2023-12-27 13:35:05 +0800

    added border-b

--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -1,6 +1,6 @@
 <div class="overflow-x-auto scrollbar">
     <div class="inline-block min-w-full align-middle">
-        <table class="min-w-full divide-y divide-gray-300">
+        <table class="min-w-full divide-y divide-gray-300 border-b">
             {{ $slot }}
         </table>
     </div>
