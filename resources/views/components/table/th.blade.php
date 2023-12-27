c71618bc7ea54e2c1ace9b42304b968e8d24c64c
Mark Makilan <mark.makilan@gets.ph>
2023-12-27 14:22:20 +0800

    added class merge

--- resources/views/components/table/th.blade.php
+++ resources/views/components/table/th.blade.php
@@ -1,1 +1,1 @@
-<th scope="col" class="p-3 text-left text-sm font-semibold text-gray-900">{{ $slot }}</th>
+<th scope="col" {{ $attributes->merge(['class' => 'p-3 text-left text-sm font-semibold text-gray-900']) }}>{{ $slot }}</th>
