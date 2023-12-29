756510efcb215cd1a4147b586221e5b4f3c841b8
Mark Makilan <mark.makilan@gets.ph>
2023-12-29 09:35:27 +0800

    ui enhancement

--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -1,6 +1,6 @@
 <div class="overflow-x-auto scrollbar">
     <div class="inline-block min-w-full align-middle">
-        <table class="min-w-full divide-y divide-gray-300 border-b">
+        <table class="min-w-full divide-y divide-gray-200">
             {{ $slot }}
         </table>
     </div>
