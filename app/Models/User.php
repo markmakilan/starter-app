7a07c6d008d83427ea9996992e9a8eb63f013ac2
Mark Makilan <mark.makilan@gets.ph>
2024-02-27 10:21:57 +0800

    Added spatie-permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,12 +9,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use App\Traits\{Uuid, ActivityLog};
 use App\Traits\User\Searchable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, Uuid, ActivityLog, Searchable;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, HasRoles, Uuid, ActivityLog, Searchable;
 
     protected $fillable = [
         'uuid',
