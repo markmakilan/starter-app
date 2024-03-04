2567fce0d5deb82a898d59cd7270ff7d3630ac33
Mark Makilan <mark.makilan@gets.ph>
2024-03-04 09:56:14 +0800

    make activity log model

--- app/Traits/ActivityLog.php
+++ app/Traits/ActivityLog.php
@@ -4,7 +4,7 @@ namespace App\Traits;
 
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Models\Activity;
+use App\Models\ActivityLog as Activity;
 
 trait ActivityLog
 {
